a1856140b55352076fe3fa703d3c42b5d778858c
Alex Edward Ball <ballalexedward@gmail.com>
2019-06-10 19:48:35 -0700

    added preprocessing step to python file

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # Automated-Essay-Grading-with-NLP
+
+#### Data Description
+The file contains 28 columns:
+
++ essay_id: A unique identifier for each individual student essay
++ essay_set: 1-8, an id for each set of essays
++ essay: The ascii text of a student's response
++ rater1_domain1: Rater 1's domain 1 score; all essays have this
++ rater2_domain1: Rater 2's domain 1 score; all essays have this
++ rater3_domain1: Rater 3's domain 1 score; only some essays in set 8 have this.
++ domain1_score: Resolved score between the raters; all essays have this
++ rater1_domain2: Rater 1's domain 2 score; only essays in set 2 have this
++ rater2_domain2: Rater 2's domain 2 score; only essays in set 2 have this
++ domain2_score: Resolved score between the raters; only essays in set 2 have this
++ rater1_trait1 score - rater3_trait6 score: trait scores for sets 7-8
+
+#### Anonymization in Essays
+
+We have made an effort to remove personally identifying information from the essays using the Named Entity Recognizer (NER) from the Stanford Natural Language Processing group and a variety of other approaches. The relevant entities are identified in the text and then replaced with a string such as "@PERSON1."
+
+The entitities identified by NER are: "PERSON", "ORGANIZATION", "LOCATION", "DATE", "TIME", "MONEY", "PERCENT"
+
+Other replacements made: "MONTH" (any month name not tagged as a date by the NER), "EMAIL" (anything that looks like an e-mail address), "NUM" (word containing digits or non-alphanumeric symbols), and "CAPS" (any capitalized word that doesn't begin a sentence, except in essays where more than 20% of the characters are capitalized letters), "DR" (any word following "Dr." with or without the period, with any capitalization, that doesn't fall into any of the above), "CITY" and "STATE" (various cities and states).
+
+Here are some hypothetical examples of replacements made:
+
++ "I attend Springfield School..." --> "...I attend @ORGANIZATION1"
++ "once my family took my on a trip to Springfield." --> "once my family took me on a trip to @LOCATION1"
++ "John Doe is a person, and so is Jane Doe. But if I talk about Mr. Doe, I can't tell that's the same person." --> "...@PERSON1 is a person, and so is @PERSON2. But if you talk about @PERSON3, I can't tell that's the same person."
++ "...my phone number is 555-2106" --> "...my phone number is @NUM1"
+
+Any words appearing in the prompt or source material for the corresponding essay set were white-listed and not anonymized.
---
+++ data_preprocessing.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+from utils.helpers import uniqueColumns, printEssaySetStats
+from data.Essay_Dicts.essay_dictionaries import essay_prompts, essay_gradeLevels, essay_sourceDependent
+
+training_essay_set = pd.read_excel('./data/training_set_rel3.xlsx')
+
+print('Total number of esseays in the dataset: {}'.format(len(training_essay_set)))
+
+# if you want to see the unique column names, run this line
+# uniqueColumns(training_essay_set)
+
+# separate essays into distinct sets
+set1 = training_essay_set[training_essay_set.essay_set == 1]
+set2 = training_essay_set[training_essay_set.essay_set == 2]
+set3 = training_essay_set[training_essay_set.essay_set == 3]
+set4 = training_essay_set[training_essay_set.essay_set == 4]
+set5 = training_essay_set[training_essay_set.essay_set == 5]
+set6 = training_essay_set[training_essay_set.essay_set == 6]
+set7 = training_essay_set[training_essay_set.essay_set == 7]
+set8 = training_essay_set[training_essay_set.essay_set == 8]
+
+# if you want to see stats from each essay set, run these lines
+# printEssaySetStats(set1)
+# printEssaySetStats(set2)
+# printEssaySetStats(set3)
+# printEssaySetStats(set4)
+# printEssaySetStats(set5)
+# printEssaySetStats(set6)
+# printEssaySetStats(set7)
+# printEssaySetStats(set8)
+
+# map dictionaries to dataframe
+training_essay_set['prompt'] = training_essay_set.essay_set.map(essay_prompts)
+training_essay_set['grade_level'] = training_essay_set.essay_set.map(essay_gradeLevels)
+training_essay_set['has_source_material'] = training_essay_set.essay_set.map(essay_sourceDependent)
+
+# read in source essay matrial for certain essays
+source3 = open('./data/sourceEssay3.txt')
+source4 = open('./data/sourceEssay4.txt')
+source5 = open('./data/sourceEssay5.txt')
+source6 = open('./data/sourceEssay6.txt')
+
+# create a source essay dictionary and map it to dataframe
+essay_sourceText = {
+    1: np.nan,
+    2: np.nan,
+    3: source3.read(),
+    4: source4.read(),
+    5: source5.read(),
+    6: source6.read(),
+    7: np.nan,
+    8: np.nan,
+}
+training_essay_set['source_text'] = training_essay_set.essay_set.map(essay_sourceText)
+
+# create one hot encoded columns for categorical data
+gradeDF = pd.get_dummies(training_essay_set['grade_level'], prefix='grade')
+
+# concat that to the dataframe and drop unnecessary column
+training_essay_set = pd.concat([training_essay_set, gradeDF], axis=1)
+training_essay_set.drop(['grade_level', 7, 8, 10], axis=1, inplace=True)
+
+# save dataframe to csv
+training_essay_set.to_csv('./data/prepped_essays_df.csv', index=False)
