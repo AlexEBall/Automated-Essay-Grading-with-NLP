9944fe8f1cc3983bc4a44f4af1f89fbe7b205667
Alex Edward Ball <ballalexedward@gmail.com>
2019-07-17 19:06:40 -0700

    saved a sgd solution and renamed a notebook

---
+++ src/models/sgd_linear_application.py
@@ -0,0 +1,13 @@
+# We set random_state=0 for reproducibility 
+linear_classifier = SGDClassifier(random_state=0)
+
+# Instantiate the GridSearchCV object and run the search
+parameters = {'alpha':[0.00001, 0.0001, 0.001, 0.01, 0.1, 1], 
+             'loss':['hinge', 'log'], 'penalty':['l1', 'l2']}
+searcher = GridSearchCV(linear_classifier, parameters, cv=10)
+searcher.fit(X_train, y_train)
+
+# Report the best parameters and the corresponding score
+print("Best CV params", searcher.best_params_)
+print("Best CV accuracy", searcher.best_score_)
+print("Test accuracy of best grid search hypers:", searcher.score(X_test, y_test))
